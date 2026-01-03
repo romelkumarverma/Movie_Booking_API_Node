fc6643b3304b70692efabb69e2dc65ecaba05c5c
Romel <romelkumarverma@gmail.com>
2026-01-03 14:15:00 +0530

    Feature: Basic Express app

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const env = require("dotenv");
+const mongoose = require("mongoose")
+
+env.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/home", (req, res) => {
+    return res.json({
+        success: true,
+        message: "Fetched home"
+    });
+});
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+
+    try{
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Successfully connected to mongodb database...")
+    } catch (err) {
+        console.log("Not able to connect...", err);
+    }
+});
